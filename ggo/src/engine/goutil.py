592c627ed80de8d37c44cfad289776da0509643b
C-Bone <curtisantolik@gmail.com>
2010-11-09 02:55:00 -0500

    Fixed a few more issues with stone placement

--- ggo/src/engine/goutil.py
+++ ggo/src/engine/goutil.py
@@ -14,9 +14,15 @@
 
 def coords_to_vertex(x,y):
     letters = [chr(a) for a in range(ord('A'),ord('U'))]
-    return letters[x]+str(y)
+    if x < 9:
+    	return letters[x-1]+str(y)
+    if x >= 9:
+	return letters[x]+str(y)
     
 def coords_from_vertex(vertex):
-    x = ord(vertex[0])-ord('A')+1
+    if ord(vertex[0]) < 74:
+    	x = ord(vertex[0])-ord('A')+2
+    else:
+	x = ord(vertex[0])-ord('A')+1
     y = int(vertex[1:])
     return (x,y) 
