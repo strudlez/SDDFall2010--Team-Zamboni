4ba315bbc798556bf1a46d4d64acad2d7ba43f4e
racarr <racarr@sarah.(none)>
2010-12-09 17:51:43 -0500

    Fix board

--- zambogo/src/engine/board.py
+++ zambogo/src/engine/board.py
@@ -1,9 +1,22 @@
-import gtp.gtp
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301,  USA
+
+import zambogo.engine.gtp.gtp
 import goutil
 
 class Board:
     def __clear_stones(self):
-		#init the list of stones
         self.stones = [[None for a in range(20)] for b in range(20)]
         
     def __parse_stones(self, color):
@@ -12,12 +25,13 @@ class Board:
         else:
             mark = 'b'
         stone_list = self.gtp.list_stones(color)
+        print stone_list
         for vertex in stone_list.split(' '):
             if len(vertex) < 2:
                 continue
-            (x,y) = goutil.vertex_to_coords(vertex)
+            (x,y) = goutil.coords_from_vertex(vertex)
             (x,y) = (x-1,y-1)
-            self.stones[x][y]=mark#keep track of the color of stone placed
+            self.stones[x][y]=mark
 
     def __update_stones(self):
         self.__clear_stones()
@@ -26,20 +40,40 @@ class Board:
     
     def __init__(self):
         #lol...
-        self.gtp = gtp.gtp.gtp()
+        self.gtp = zambogo.engine.gtp.gtp.gtp()
 
         self.gtp.set_boardsize("19")
         self.gtp.clear_board()
         self.__clear_stones()
-        
-    def undo(self):
+
+    def estimate_score(self):
         try:
-            self.gtp.undo()
+            score = self.gtp.estimate_score()
         except RuntimeError:
             return False
-        self.__update_stones()
-        return True
-        
+        return score
+
+    def final_score(self):
+        try:
+            score = self.gtp.final_score()
+        except RuntimeError:
+            return False
+        return score
+
+    def set_time(self, time, bytime):
+        try:
+            tim = self.gtp.time_settings(time, bytime, 30)
+        except RuntimeError:
+            return False
+        return tim
+
+    def get_time(self, color):
+        try:
+            timelft = self.gtp.time_left(color)
+        except RuntimeError:
+            return False
+        return timelft
+     
     def make_move(self, color, x, y):
         vertex = goutil.coords_to_vertex(x,y)
         try: 
@@ -47,5 +81,12 @@ class Board:
         except RuntimeError:
             return False
         self.__update_stones()
+        return True
+            
+    def make_gnugo_move(self, color, callback):
+        def gnugo_done(response):
+            self.__update_stones()
+            callback(response)
+        self.gtp.genmove(color, gnugo_done)
         return True
         
